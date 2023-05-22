09478d54da7674d3b7edd571eb02c946503067b9
so_hell <armanyppph@gmail.com>
2023-05-23 02:53:10 +0530

    calculator

--- index.php
+++ index.php
@@ -7,91 +7,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP</title>
 </head>
-
 <body>
-
-
-    <form method="GET">
-        <input type="text" name="person">
-        <button>Submit</button>
+    <form>
+        <input type="text" name="num1" placeholder="Number 1" />
+        <input type="text" name="num2" placeholder="Number 2" />
+        <select name="operator">
+            <option value="add">Add</option>
+            <option value="subtract">Subtract</option>
+            <option value="multiply">Multiply</option>
+            <option value="divide">Divide</option>
+        </select>
+        <br/>
+        <button type="submit" name="submit" value="submit">Calculate</button>
     </form>
-
-    <div>
+    <p>The answer is:  
         <?php
-        // $name = $_GET['person'];
-        // echo "My name is " . $name;
-        
-
-        //Functions
-        
-        // 1) in-nbuilt functions
-        echo strlen("Hi There!") . "\n";
-        echo strpos("Hi There", "Hi");
-
-        // Data types in PHP
-        $name = "This is my string!";
-        $name = 20;
-        $name = 20.54;
-        $name = FALSE;
-        $name = array("Md", "Sohel", 1);
-        echo $name[0];
-
-        // operators in php
-        
-        echo 5 + 5;
-
-        // comparision
-        
-        $x = 5;
-        $y = 5;
-
-        if ($x == $y) {
-            echo "True!";
-        } else {
-            echo "False!";
-        }
-
-        // Increment/Decrement
-        
-        $x = 10;
-        $x++;
-        echo $x;
-
-
-        // Logical Operators
-        
-        $x = 10;
-        $y = 20;
-
-        if (($x == $y && false) || true) {
-            echo "True!";
-        } else {
-            echo "False!";
-        }
-
-        // Switch in PHP
-        
-
-        $x = 1;
-
-        switch ($x) {
-            case 1:
-                echo "The value of x is " . 1;
-                break;
-            case 2:
-                echo "The value of x is " . 2;
-                break;
-            case "number":
-                echo "The value of x is " . "number";
-                break;
-            default:
-                echo "Nothing found";
-        }
-
-
-
+            if(isset($_GET['submit'])){
+                $result1 = $_GET['num1'];
+                $result2 = $_GET['num2'];
+                $opeator = $_GET['operator'];
+                switch($opeator){
+                    case "add" :
+                        echo $result1 + $result2;
+                        break;
+                    case "subtract":
+                        echo $result1 - $result2;
+                        break;
+                    case "multiply":
+                        echo $result1 * $result2;
+                        break;
+                    case "divide":
+                        echo $result1 / $result2;
+                        break;
+                }
+            }
+            else {
+                echo "Fill the form first!!";
+            }
         ?>
-    </div>
+    </p>
 </body>
 
 </html>
