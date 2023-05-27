bc2a0a4b2d0a98f3ffb549ee213c5d72ee5f7c1d
so_hell <armanyppph@gmail.com>
2023-05-27 06:44:30 +0530

    learning php

---
+++ array.php
@@ -0,0 +1,7 @@
+<?php
+    $myArr = array("First Element" , 2 , 'a');
+    for($index = 0; $index < count($myArr); $index++) {
+        echo $myArr[0] . ' ';
+    }
+
+?>
---
+++ function.php
@@ -0,0 +1,10 @@
+<?php
+    function addTwoNumbers($num1 , $num2) {
+        return $num1 + $num2;
+    }
+    echo addTwoNumbers(1,2);
+    function newCalc($x){
+        return $x * 0.75;
+    }
+    echo newCalc(50);
+?>  
